af798f9d4764fca2607127d54bf7e7c5eee3bfc2
skhandavalli <s.khandavalli@thoughtworks.com>
2021-02-09 21:21:21 +0530

    Add the environment variable for the Api key to use youtube api and Jest setup for the same

--- src/services/youtubeApi.js
+++ src/services/youtubeApi.js
@@ -1,11 +1,11 @@
 import axios from 'axios';
-const KEY = 'Enter your Api Key here';
+const API_KEY = process.env.REACT_APP_API_KEY;
 
 export default axios.create({
     baseURL: 'https://www.googleapis.com/youtube/v3/',
     params: {
         part: 'snippet',
-        maxResults: 10,
-        key: KEY
+        maxResults: 5,
+        key: API_KEY
     }
 })
--- src/services/youtubeApi.test.js
+++ src/services/youtubeApi.test.js
@@ -2,6 +2,6 @@ import youtubeApi from './youtubeApi';
 describe('youtubeApi',() => {
   it('should initialize fetch with the right youtube api defaults', () => {
     expect(youtubeApi.defaults.baseURL).toBe('https://www.googleapis.com/youtube/v3/');
-    expect(youtubeApi.defaults.params).toStrictEqual({key: "Enter your Api Key here", maxResults: 10, part: "snippet"})
+    expect(youtubeApi.defaults.params).toStrictEqual({key: "Google Api Key", maxResults: 5, part: "snippet"})
   })
 })
